0511c18fa998c9ecc9b7e2314b6e536a77e2dcc8
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2023-01-23 21:01:53 -0500

    commit

--- util/database.js
+++ util/database.js
@@ -1,13 +1,23 @@
-// mysql connection script
+/* mysql connection script
 
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  port: "3301",
+// const pool = mysql.createPool({
+//   port: "3301",
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "yejiahao1234",
+// });
+
+// module.exports = pool.promise(); */
+
+// connect to mySQL database using sequelize:
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "yejiahao1234", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "yejiahao1234",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
